0591d3b26a0cf811678f8c623e54857c7342202f
Taturick <noturno2drick@gmail.com>
2023-08-16 17:09:06 -0300

    apresentando Sort

--- sort.js
+++ sort.js
@@ -1,7 +1,37 @@
-const numeros = [1,2,5,4,8,55,664,3135,20];
+const numeros = [1, 2, 5, 4, 8, 55, 664, 3135, 20];
+// primeiro elemento A, segundo elemento B
 
-const ordernador = numeros.sort((a,b)=>{
-    return a -b;
+numeros.sort((a, b) => {
+    return a - b;
+});
+console.log(numeros);
+
+// Retorna ordem descrescente
+numeros.sort((a,b)=>{
+    return b -a;
 });
 
-console.log(ordernador);
+console.log(numeros);
+
+numeros.sort((a, b) => {
+    if (a < b) {
+        return -1;
+    }
+    if (a > b) {
+        return 1;
+    }
+    return 0;
+});
+
+console.log(numeros)
+
+numeros.sort((a, b) => {
+    if (a < b) {
+        return 1;
+    }
+    if (a > b) {
+        return -1;
+    }
+    return 0;
+});
+console.log(numeros)
